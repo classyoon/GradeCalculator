cc533aeeedbe89b92d5c23cadf7c789f6997c3b7
classyoon <96508736+classyoon@users.noreply.github.com>
2024-06-07 21:17:53 -0400

    Initial Commit

---
+++ GradeCalculatorUITests/GradeCalculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GradeCalculatorUITestsLaunchTests.swift
+//  GradeCalculatorUITests
+//
+//  Created by Conner Yoon on 6/7/24.
+//
+
+import XCTest
+
+final class GradeCalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
